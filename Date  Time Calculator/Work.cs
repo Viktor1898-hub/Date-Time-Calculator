44b4fc167483c1c45a1fe813c7015db097fb75c0
Viktor1898-hub <duzhik1898@gmail.com>
2025-11-22 03:12:16 +0100

    Add Setting class and Work class

---
+++ Date  Time Calculator/Work.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date__Time_Calculator
+{
+    internal class Work
+    {
+    }
+}
